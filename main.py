6803706fe71272b593f0f6b22d84baaee0d6045b
Lydie A <lydie.archidoit@orange.fr>
2023-06-10 01:08:15 +0200

    update

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ from Player import *
 
 #création fenetre principale
 fenetre=Tk()
-fenetre.geometry('850x538')
+
+fenetre.geometry('1000x600+100+10')
 
 
 #frame pour choisir la difficulté
@@ -43,7 +44,7 @@ framejouer.place(x=200, y=30)
 
 #frame bouton abandonner
 frameabandonner=Frame(fenetre)
-frameabandonner.place(x=200, y=480)
+frameabandonner.place(x=300, y=30)
 
 #compteur de vie
 compteur = 3
@@ -81,7 +82,7 @@ def abandonner():
     for cle in listentry:
         cle.config(state='disable',disabledbackground="#ff556c")
     gameover=Toplevel(fenetre)
-    gameover.geometry("500x160")
+    gameover.geometry("500x160+380+190")
     lab3=Label(gameover, text="GAME OVER", font=('impact',50), fg='red')
     lab3.pack()
     perdu()
@@ -116,25 +117,25 @@ def jouer(diff:float=0.5, taille:int=3) -> None:
     global lab
     lab=[] #liste contenant les labels
     frameprincipale=Frame(fenetre)
-    frameprincipale.place(x=500, y=170)
+    frameprincipale.place(x=440, y=50)
     blanc=0
     nbcolumn=0
     for j in range(9):
         nbrow=0
         for i in range(9):
-            l=Label(frameprincipale,bg='#858585',width=4,height=2,relief="groove",borderwidth=4)
+            l=Label(frameprincipale,bg='#858585',width=6,height=3,relief="groove",borderwidth=4)
             l.grid(row=nbrow,column=nbcolumn)
             lab.append([l,(j,i)]) 
             blanc+=1
             if blanc%3==0:
                 nbrow+=1
-                lablanc=Label(frameprincipale,  text='')
+                lablanc=Label(frameprincipale,  text='', width=1,height=1)
                 lablanc.grid(row=nbrow,column=nbcolumn)
             nbrow+=1
         if blanc==27 or blanc==54:
             for it in range(11):
                 nbcolumn+=1
-                lablanc2=Label(frameprincipale, text='')
+                lablanc2=Label(frameprincipale, text='',width=1,height=1)
                 lablanc.grid(row=nbrow,column=nbcolumn)
         nbcolumn+=1
             
@@ -148,8 +149,8 @@ def jouer(diff:float=0.5, taille:int=3) -> None:
         label=l[0]
         texte=str(remplir(sudoku)[0])
         if texte=='0':
-            entree=Entry(label, width=0, bg='#a1a1a1')
-            entree.grid(ipadx=10, ipady=6)
+            entree=Entry(label, bg='#a1a1a1',cursor='cross')
+            entree.place(width=44,height=45)
             listentry[entree]=l[1]
         else:
             label['text'] += texte
@@ -175,7 +176,7 @@ def jouer(diff:float=0.5, taille:int=3) -> None:
                     if len(listentry)==1:
                         boutjouer.config(state='normal')
                         gagne=Toplevel(fenetre)
-                        gagne.geometry("500x160")
+                        gagne.geometry("500x160+380+190")
                         lab4=Label(gagne, text="PARTIE GAGNE", font=('impact',50), fg='green')
                         lab4.pack()
                     del(listentry[cle])
@@ -188,8 +189,8 @@ def jouer(diff:float=0.5, taille:int=3) -> None:
                             #ajouter variable qui stock les vicctoire et les défaites
                     else:
                         faux=Toplevel(fenetre)
-                        faux.geometry("200x200")
-                        lab=Label(faux, text=f"Mauvaise Réponse il vous reste : {compteur} mauvaises réponses", font='impact',width=400)
+                        faux.geometry("400x200+380+190")
+                        lab=Label(faux, text=f"Mauvaise Réponse \n\n Il vous reste {compteur} vie(s)", font='arial',width=400)
                         lab.pack()
         
                     
@@ -204,6 +205,14 @@ boutjouer=Button(framejouer, bg='#45e325', text='Jouer', fg='white', font='Impac
 boutjouer.grid()
 
 
+canvas = Canvas(fenetre, width=300, height=300, bg='ivory', borderwidth=0, highlightthickness=0)
+canvas.place(x=60,y=150)
+
+canvas.create_text(150, 20, text= "Info joueur",fill="black",font=('Helvetica 12 bold italic'))
+canvas.create_text(70, 60, text= "Partie gagnées :",fill="black",font=('arial 11'))
+canvas.create_text(70, 90, text= "Partie perdues :",fill="black",font=('arial 11'))
+canvas.create_text(70, 120, text= "Points :",fill="black",font=('arial 11 '))
+canvas.create_text(70, 150, text=f"Vie restante : {compteur}",fill="black",font=('arial 11'))
 
 
 
@@ -254,63 +263,7 @@ def ouvrirResolution():
     genere()
     
 
-def facile():
-    global grilleSudo,typeJeu,modeJeu
-    t.delete("0.0",END)
-    can.delete(ALL)
-    grilleSudo=grilleFacile
-    typeJeu=1
-    modeJeu=0
-    bdesactive.configure(state=DISABLED)
-    bactive.configure(state=ACTIVE)
-    genere()
 
-def moyen():
-    global grilleSudo,typeJeu,modeJeu
-    t.delete("0.0",END)
-    can.delete(ALL)
-    grilleSudo=grilleMoyen
-    typeJeu=1
-    modeJeu=0
-    bdesactive.configure(state=DISABLED)
-    bactive.configure(state=ACTIVE)
-    genere()
-
-def difficile():
-    global grilleSudo,typeJeu,modeJeu
-    t.delete("0.0",END)
-    can.delete(ALL)
-    grilleSudo=grilleDifficile
-    typeJeu=1
-    modeJeu=0
-    bdesactive.configure(state=DISABLED)
-    bactive.configure(state=ACTIVE)
-    genere()
-
-def diabolique():
-    global grilleSudo,typeJeu,modeJeu
-    t.delete("0.0",END)
-    can.delete(ALL)
-    grilleSudo=grilleDiabolique
-    typeJeu=1
-    modeJeu=0
-    bdesactive.configure(state=DISABLED)
-    bactive.configure(state=ACTIVE)
-    genere()
-
-def genere():
-    global grilleSudo,typeJeu,bloqueSudo
-    bloqueSudo=[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    t.delete("0.0",END)
-    can.delete(ALL)
-    for i in range(nb):
-            for j in range(nb):
-                if bloqueSudo[i][j]!=grilleSudo[i][j]:
-                    bloqueSudo[i][j]+=10
-    typeJeu=1
-    grille()
 
 def bactive():
     global modeJeu
@@ -332,8 +285,8 @@ def menuAPropos():
      fen.geometry("500x500")
      lab=Label(fen, text="Voici les règles du Sudoku \n Il faut remplir les cases. \n Bonne chance!\n",font='arial',width=100)
      lab.pack() 
-     lab['text','font'] +=( 'Par Elouan, Léopold et Lydie' ,'impact')                                                
-
+     lab1=Label(fen, text='Par Elouan, Léopold et Lydie', font=("Times New Roman", 10, "italic"))
+     lab1.pack()
 
 # stop_quit permet de fermer et détruire la fenetre
 
@@ -356,10 +309,7 @@ menuFichier.add_command(label="Quitter", command=stop_quit)
 
 menuResolution=Menu(barreDeMenus)                                                             
 barreDeMenus.add_cascade(label="Menu 1", menu=menuResolution)
-menuResolution.add_command(label="Grille Facile", command=facile)
-menuResolution.add_command(label="Grille Moyen", command=moyen)
-menuResolution.add_command(label="Grille Difficile", command=difficile)
-menuResolution.add_command(label="Grille Diabolique", command=diabolique)
+menuResolution.add_command(label="Abandonner", command=abandonner)
 menuResolution.add_separator()                                                                  
 menuResolution.add_command(label="Ouvrir une grille en mode résolution", command=ouvrirResolution)
 
